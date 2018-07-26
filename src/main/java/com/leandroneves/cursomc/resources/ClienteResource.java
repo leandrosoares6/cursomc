a87426379c8ee4dcef79eed7af8191fc599ee498
Leandro Soares Neves <leandro.neves@tce.pi.gov.br>
2018-07-26 15:38:39 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/leandroneves/cursomc/resources/ClienteResource.java
+++ src/main/java/com/leandroneves/cursomc/resources/ClienteResource.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.leandroneves.cursomc.domain.Categoria;
-import com.leandroneves.cursomc.services.CategoriaService;
+import com.leandroneves.cursomc.domain.Cliente;
+import com.leandroneves.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 																																																																																																																																																																																																																																																								
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> listar(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
